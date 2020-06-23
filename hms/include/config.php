cc739d432c2e35ecbb3663fa04473cff5f23d153
Akarsh T S <ts.akarsh@gmail.com>
2020-06-23 15:00:32 +0600

    "Added remote mysql server"

--- hms/include/config.php
+++ hms/include/config.php
@@ -1,8 +1,8 @@
 <?php
-$mysql_hostname = "localhost";
-$mysql_user = "root";
-$mysql_password = "";
-$mysql_database = "hms";
+$mysql_hostname = "sql12.freemysqlhosting.net";
+$mysql_user = "sql12350266";
+$mysql_password = "kd9SbGtdFn";
+$mysql_database = "sql12350266";
 $bd = @mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Could not connect database");
 mysql_select_db($mysql_database,$bd) or die("Could not select database");
 
